d6cd99cca5cd4b37029e193fcf2169fd99e0fc50
heyMP <mgp140@gmail.com>
2018-10-17 12:53:12 -0400

    corrected symlink

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Creating odl-haxtheme symlink in global webcomponents/bower_components file."
-ln -sf theme/odl-haxtheme webcomponents/bower_components/odl-haxtheme
+ln -sf ../../odl/theme/odl-haxtheme ../../webcomponents/bower_components/odl-haxtheme
