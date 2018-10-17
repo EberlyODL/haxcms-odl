6436d6e54682046d7e9d9a814992b65554504bd9
heyMP <mgp140@gmail.com>
2018-10-17 12:28:13 -0400

    updated readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# ODL Website
+
+## Install
+
+```bash
+cd haxcms/_sites/
+git clone https://github.com/EberlyODL/haxcms-odl.git odl
+cd odl
+bash setup.sh
+```
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Creating odl-haxtheme symlink in global webcomponents/bower_components file."
+ln -sf theme/odl-haxtheme webcomponents/bower_components/odl-haxtheme
