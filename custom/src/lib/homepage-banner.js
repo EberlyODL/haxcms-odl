45301974486c3cfab4132790e061cf971721163a
cgldevel <cgldevel@gmail.com>
2020-03-02 11:49:30 -0500

    adjusted homepage logo for better mobile response

--- custom/src/lib/homepage-banner.js
+++ custom/src/lib/homepage-banner.js
@@ -144,6 +144,12 @@ class HomePageBanner extends PolymerElement {
           @apply --haxtheme-homepage-banner-logo-image;
         }
 
+        @media screen and (max-width: 700px) {
+          .logo img {
+            margin: 0 0 0 20px;
+          }
+        }
+
         .company_name {
           width: var(--haxtheme-homepage-banner-company-name-width, 76%);
           margin: var(
