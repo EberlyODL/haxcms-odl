6bb4299533f56d64d6348e61a31c72104f8fc17b
cgldevel <cgldevel@gmail.com>
2020-04-14 16:19:32 -0400

    added stop-note.js import to odlhaxtheme base so it would show up on the page. Similar issue we've been running into.

--- custom/src/odl-haxtheme.js
+++ custom/src/odl-haxtheme.js
@@ -10,6 +10,7 @@ import { dom } from "@polymer/polymer/lib/legacy/polymer.dom.js";
 import { wipeSlot } from "@lrnwebcomponents/hax-body/lib/haxutils.js";
 import "@lrnwebcomponents/media-image/media-image.js";
 import "@lrnwebcomponents/citation-element/citation-element.js";
+import "@lrnwebcomponents/stop-note/stop-note.js";
 import "@polymer/iron-pages/iron-pages.js";
 import "./lib/haxtheme-home.js";
 import "./lib/haxtheme-about.js";
